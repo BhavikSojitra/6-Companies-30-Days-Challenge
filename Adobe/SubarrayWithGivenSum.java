1c490bbbcc2ef3415212d34aa1261147f2ea573e
Bhavik Sojitra <75220689+BhavikSojitra@users.noreply.github.com>
2022-01-31 17:24:50 +0530

    Update SubarrayWithGivenSum.java

--- Adobe/SubarrayWithGivenSum.java
+++ Adobe/SubarrayWithGivenSum.java
@@ -1,1 +1,29 @@
-
+class Solution{
+    static ArrayList<Integer> subarraySum(int[] arr, int n, int s) {
+        int idx=0, i=idx, sum=0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(i<arr.length){
+            sum+=arr[i];
+            if(sum<s){
+                i++;
+                continue;
+            }
+            if(sum>s){
+                idx++;
+                i=idx;
+                sum=0;
+            }
+            if(sum==s){
+                ans.add(idx+1);
+                ans.add(i+1); 
+                break;
+            }
+        }
+        if(ans.isEmpty()){
+            ans.add(-1);
+            return ans;
+        }else{
+            return ans;
+        }
+    }
+}
